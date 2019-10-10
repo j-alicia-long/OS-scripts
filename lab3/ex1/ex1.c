522444e2076fc65e87cefaee22072c83b63ea527
Jennifer Long <j.alicia.long@gmail.com>
2019-10-10 11:07:51 +0800

    no starvation

--- lab3/ex1/ex1.c
+++ lab3/ex1/ex1.c
@@ -11,7 +11,6 @@
 void initialise(rw_lock* lock)
 {
   pthread_mutex_init(&(lock->rmutex), NULL);
-  pthread_mutex_init(&(lock->wmutex), NULL);
   pthread_mutex_init(&(lock->roomEmpty), NULL);
   lock->reader_count = 0;
   lock->writer_count = 0;
@@ -21,16 +20,12 @@ void writer_acquire(rw_lock* lock)
 {
   pthread_mutex_lock(&(lock->roomEmpty));
 
-  pthread_mutex_lock(&(lock->wmutex));
   lock->writer_count++;
-  pthread_mutex_unlock(&(lock->wmutex));
 }
 
 void writer_release(rw_lock* lock)
 {
-  pthread_mutex_lock(&(lock->wmutex));
   lock->writer_count--;
-  pthread_mutex_unlock(&(lock->wmutex));
 
   pthread_mutex_unlock(&(lock->roomEmpty));
 }
@@ -39,23 +34,24 @@ void reader_acquire(rw_lock* lock)
 {
   pthread_mutex_lock(&(lock->rmutex));
   lock->reader_count++;
-  if(lock->reader_count == 1)
-    pthread_mutex_lock(&(lock->roomEmpty));
   pthread_mutex_unlock(&(lock->rmutex));
+
+  if(lock->reader_count == 1)
+    pthread_mutex_lock(&(lock->roomEmpty));  
 }
 
 void reader_release(rw_lock* lock)
 {
   pthread_mutex_lock(&(lock->rmutex));
   lock->reader_count--;
+  pthread_mutex_unlock(&(lock->rmutex));
+
   if(lock->reader_count == 0)
     pthread_mutex_unlock(&(lock->roomEmpty));
-  pthread_mutex_unlock(&(lock->rmutex));
 }
 
 void cleanup(rw_lock* lock)
 {
   pthread_mutex_destroy(&(lock->rmutex));
-  pthread_mutex_destroy(&(lock->wmutex));
   pthread_mutex_destroy(&(lock->roomEmpty));
 }
--- lab3/ex2/ex2.c
+++ lab3/ex2/ex2.c
@@ -13,6 +13,7 @@ void initialise(rw_lock* lock)
   pthread_mutex_init(&(lock->rmutex), NULL);
   pthread_mutex_init(&(lock->wmutex), NULL);
   pthread_mutex_init(&(lock->readGate), NULL);
+  pthread_mutex_init(&(lock->queue), NULL);
   pthread_mutex_init(&(lock->roomEmpty), NULL);
   lock->reader_count = 0;
   lock->writer_count = 0;
@@ -20,34 +21,40 @@ void initialise(rw_lock* lock)
 
 void writer_acquire(rw_lock* lock)
 {
+  pthread_mutex_lock(&(lock->queue));
+  
   pthread_mutex_lock(&(lock->roomEmpty));
 
-  pthread_mutex_lock(&(lock->wmutex));
+  pthread_mutex_unlock(&(lock->queue));
+
+ // pthread_mutex_lock(&(lock->wmutex));
   lock->writer_count++;
-  if(lock->writer_count == 1) // if you are first writer
-    pthread_mutex_lock(&(lock->readGate)); //lock out the readers
-  pthread_mutex_unlock(&(lock->wmutex));
+ // pthread_mutex_unlock(&(lock->wmutex));
 }
 
 void writer_release(rw_lock* lock)
 {
-  pthread_mutex_lock(&(lock->wmutex));
+ // pthread_mutex_lock(&(lock->wmutex));
   lock->writer_count--;
-  if(lock->writer_count == 0) // if you are last writer
-    pthread_mutex_unlock(&(lock->readGate)); //unlock the readers
-  pthread_mutex_unlock(&(lock->wmutex));
+ // pthread_mutex_unlock(&(lock->wmutex));
 
   pthread_mutex_unlock(&(lock->roomEmpty));
 }
 
 void reader_acquire(rw_lock* lock)
 {
+  pthread_mutex_lock(&(lock->queue)); 
+  pthread_mutex_unlock(&(lock->queue));
+
   pthread_mutex_lock(&(lock->readGate));
+
+  if(lock->reader_count == 0)
+    pthread_mutex_lock(&(lock->roomEmpty));
+
   pthread_mutex_lock(&(lock->rmutex));
   lock->reader_count++;
-  if(lock->reader_count == 1)
-    pthread_mutex_lock(&(lock->roomEmpty));
   pthread_mutex_unlock(&(lock->rmutex));
+  
   pthread_mutex_unlock(&(lock->readGate));
 }
 
@@ -55,9 +62,12 @@ void reader_release(rw_lock* lock)
 {
   pthread_mutex_lock(&(lock->rmutex));
   lock->reader_count--;
+  pthread_mutex_unlock(&(lock->rmutex));
+
   if(lock->reader_count == 0)
     pthread_mutex_unlock(&(lock->roomEmpty));
-  pthread_mutex_unlock(&(lock->rmutex));
+  
+  pthread_mutex_unlock(&(lock->readGate));
 }
 
 void cleanup(rw_lock* lock)
